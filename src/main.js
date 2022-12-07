4f9d347c1d32bd4d6bebaf919b2edb2b84f589bb
euss99 <euss99@hotmail.com>
2022-12-07 00:55:15 -0600

    Creando la lista de categorías

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@
 const API = "https://api.themoviedb.org/3/";
 const api_key = `api_key=` + API_KEY;
 // API trending
-const endpoint = "trending/movie/day";
-const API_TRENDING = `${API}${endpoint}?${api_key}`;
+const endpoint_TRENDING = "trending/movie/day";
+const API_TRENDING = `${API}${endpoint_TRENDING}?${api_key}`;
+// API genres
+const endpoint_GENRE = "/genre/movie/list"
+const API_GENRE = `${API}${endpoint_GENRE}?${api_key}`;
 
 // Función para conseguir las películas en tendencia para preview
 async function getTrendingMoviesPreview() {
@@ -11,9 +14,10 @@ async function getTrendingMoviesPreview() {
     const data = await res.json();
 
     const movies = data.results;
-    movies.forEach(movie => {
-        const trendingPreviewMoviesContainer = document.querySelector("#trendingPreview .trendingPreview-movieList");
 
+    const trendingPreviewMoviesContainer = document.querySelector("#trendingPreview .trendingPreview-movieList");
+    movies.forEach(movie => {
+        
         const movieContainer = document.createElement("div");
         movieContainer.classList.add("movie-container");
 
@@ -31,4 +35,28 @@ async function getTrendingMoviesPreview() {
     });
 }
 
+// Función para ver las categorias de las películas
+async function getCategoriesPreview() {
+    const res = await fetch(API_GENRE);
+    const data = await res.json();
+
+    const categorias = data.genres;
+
+    const previewCategoriesContainer = document.querySelector("#categoriesPreview .categoriesPreview-list")
+    categorias.forEach(category => {
+        const categoryContainer = document.createElement("div");
+        categoryContainer.classList.add("category-container");
+
+        const categoryTitle = document.createElement("h3");
+        categoryTitle.classList.add("category-title");
+        categoryTitle.setAttribute("id", "id" + category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoriesContainer.appendChild(categoryContainer);
+    })
+}
+
 getTrendingMoviesPreview();
+getCategoriesPreview();
